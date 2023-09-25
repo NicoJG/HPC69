7c0ba999ebda0352ad78934531d17c4d60e0c7f7
Rémi Albert <albert.remi@gmail.com>
2023-09-25 14:41:26 +0200

    Placed the computation and merging of the char in a function 'merge_char'

--- read_file.c
+++ read_file.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 
 #define BUFFER_SZ 512
-#define COORD_NBR 5
+#define COORD_NBR 5 // Could change to BUFFER_SZ / ( 3 * 8 )
+#define FLOAT_LENGTH 8
+
+
+int merge_char(char *buffer, size_t ix, int which_coord);
 
 int 
 main (
@@ -23,68 +27,16 @@ main (
     fseek(fp, 0, SEEK_SET);
     fread(buffer, sizeof(buffer), 1, fp);
 
-    int float_length = 8;
-    int base, bbase, dec, ddec, dddec, sign;
 
-    int *x = (int *) malloc(sizeof(int) * 20);
-    int *y = (int *) malloc(sizeof(int) * 20);
-    int *z = (int *) malloc(sizeof(int) * 20);
+    int *x = (int *) malloc(sizeof(int) * COORD_NBR);
+    int *y = (int *) malloc(sizeof(int) * COORD_NBR);
+    int *z = (int *) malloc(sizeof(int) * COORD_NBR);
 
-    for (size_t ix, jx = 0; ix <= 3* COORD_NBR * float_length; ix += 3 * float_length, jx++) {
-
-	// x
-        if (buffer[ix] == '-') {
-            sign = -1;
-        }
+    for (size_t ix = 0, jx = 0; ix <= 3* COORD_NBR * FLOAT_LENGTH; ix += 3 * FLOAT_LENGTH, jx++) {
         
-        base = *(buffer + ix + 1) - '0';
-        bbase = *(buffer + ix + 2) - '0';
-        dec = *(buffer + ix + 4) - '0';
-        ddec = *(buffer + ix + 5) - '0';
-        dddec = *(buffer + ix + 6) - '0';
-
-        x[jx] = sign * (
-            base * 10000 + 
-            bbase * 1000 +
-            dec * 100 + 
-            ddec * 10 +
-            dddec);
-        
-	// y
-        if (buffer[ix + float_length] == '-') {
-            sign = -1;
-        }
-
-        base = *(buffer + ix + 1 + float_length) - '0';
-        bbase = *(buffer + ix + 2 + float_length) - '0';
-        dec = *(buffer + ix + 4 + float_length) - '0';
-        ddec = *(buffer + ix + 5 + float_length) - '0';
-        dddec = *(buffer + ix + 6 + float_length) - '0';
-
-        y[jx] = sign * (
-            base * 10000 + 
-            bbase * 1000 +
-            dec * 100 + 
-            ddec * 10 +
-            dddec);
-
-	// z
-        if (buffer[ix + 2 * float_length] == '-') {
-            sign = -1;
-        }
-
-        base = *(buffer + ix + 1 + 2 * float_length) - '0';
-        bbase = *(buffer + ix + 2 + 2 * float_length) - '0';
-        dec = *(buffer + ix + 4 + 2 * float_length) - '0';
-        ddec = *(buffer + ix + 5 + 2 * float_length) - '0';
-        dddec = *(buffer + ix + 6 + 2 * float_length) - '0';
-
-        z[jx] = sign * (
-            base * 10000 + 
-            bbase * 1000 +
-            dec * 100 + 
-            ddec * 10 +
-            dddec);
+	x[jx] = merge_char(buffer, ix, 0);
+	y[jx] = merge_char(buffer, ix, 1);
+	z[jx] = merge_char(buffer, ix, 2);
     }
     
     for (size_t ix = 0; ix < COORD_NBR; ix++) {
@@ -101,4 +53,28 @@ main (
 
     return 0;
 
+}
+
+int merge_char(char *buffer, size_t ix, int which_coord){
+
+    	int base, bbase, dec, ddec, dddec, sign = 1, num;
+        
+	if (buffer[ix + which_coord * FLOAT_LENGTH] == '-') {
+            sign = -1;
+        }
+
+        base = *(buffer + ix + which_coord * FLOAT_LENGTH + 1) - '0';
+        bbase = *(buffer + ix + which_coord * FLOAT_LENGTH + 2 ) - '0';
+        dec = *(buffer + ix + which_coord * FLOAT_LENGTH + 4 ) - '0';
+        ddec = *(buffer + ix + which_coord * FLOAT_LENGTH + 5 ) - '0';
+        dddec = *(buffer + ix + which_coord * FLOAT_LENGTH + 6 ) - '0';
+
+        num = sign * (
+            base * 10000 + 
+            bbase * 1000 +
+            dec * 100 + 
+            ddec * 10 +
+            dddec);
+
+	return num;
 }
