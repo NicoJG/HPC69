054042281c8a632214c33bc58f6cdf430dff10ef
Rémi Albert <albert.remi@gmail.com>
2023-09-25 14:14:58 +0200

    Refactored to get rid of if statements by making use of fixed x, y, z format in input data

--- read_file.c
+++ read_file.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #define BUFFER_SZ 512
+#define COORD_NBR 5
 
 int 
 main (
@@ -23,18 +24,15 @@ main (
     fread(buffer, sizeof(buffer), 1, fp);
 
     int float_length = 8;
-    int base, bbase, dec, ddec, dddec, sign, num;
-    int coord = 0;
-    int count_x = 0;
-    int count_y = 0;
-    int count_z = 0;
+    int base, bbase, dec, ddec, dddec, sign;
 
-    int *x = (int *) malloc(sizeof(int) * 100);
-    int *y = (int *) malloc(sizeof(int) * 100);
-    int *z = (int *) malloc(sizeof(int) * 100);
+    int *x = (int *) malloc(sizeof(int) * 20);
+    int *y = (int *) malloc(sizeof(int) * 20);
+    int *z = (int *) malloc(sizeof(int) * 20);
 
-    for (size_t ix = 0; ix <= 30 * float_length; ix += float_length) {
+    for (size_t ix, jx = 0; ix <= 3* COORD_NBR * float_length; ix += 3 * float_length, jx++) {
 
+	// x
         if (buffer[ix] == '-') {
             sign = -1;
         }
@@ -45,37 +43,62 @@ main (
         ddec = *(buffer + ix + 5) - '0';
         dddec = *(buffer + ix + 6) - '0';
 
-        num = sign * (
+        x[jx] = sign * (
             base * 10000 + 
             bbase * 1000 +
             dec * 100 + 
             ddec * 10 +
             dddec);
         
-        sign = 1;
-        
-        if (coord == 0) {
-            x[count_x] = num;
-            count_x++;
-            coord +=1;
-        } else if (coord == 1) {
-            y[count_y] = num;
-            count_y++;
-            coord += 1;
-        } else {
-            z[count_z] = num;
-            count_z++;
-            coord = 0;
+	// y
+        if (buffer[ix + float_length] == '-') {
+            sign = -1;
         }
+
+        base = *(buffer + ix + 1 + float_length) - '0';
+        bbase = *(buffer + ix + 2 + float_length) - '0';
+        dec = *(buffer + ix + 4 + float_length) - '0';
+        ddec = *(buffer + ix + 5 + float_length) - '0';
+        dddec = *(buffer + ix + 6 + float_length) - '0';
+
+        y[jx] = sign * (
+            base * 10000 + 
+            bbase * 1000 +
+            dec * 100 + 
+            ddec * 10 +
+            dddec);
+
+	// z
+        if (buffer[ix + 2 * float_length] == '-') {
+            sign = -1;
+        }
+
+        base = *(buffer + ix + 1 + 2 * float_length) - '0';
+        bbase = *(buffer + ix + 2 + 2 * float_length) - '0';
+        dec = *(buffer + ix + 4 + 2 * float_length) - '0';
+        ddec = *(buffer + ix + 5 + 2 * float_length) - '0';
+        dddec = *(buffer + ix + 6 + 2 * float_length) - '0';
+
+        z[jx] = sign * (
+            base * 10000 + 
+            bbase * 1000 +
+            dec * 100 + 
+            ddec * 10 +
+            dddec);
     }
     
-    for (size_t ix = 0; ix < 100; ix++) {
+    for (size_t ix = 0; ix < COORD_NBR; ix++) {
         printf("%d, %d, %d\n", x[ix], y[ix], z[ix]);
     }
     fseek(fp, 0, SEEK_END);
     printf("The file has size %li\n", ftell(fp));
   
     fclose(fp);
+    
+    free(x);
+    free(y);
+    free(z);
+
     return 0;
 
 }
