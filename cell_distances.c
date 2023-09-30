4ede8c59c1f006c4db8b2d2251b8e179b37f2aa7
Nico Guth <nicog@chalmers.se>
2023-09-30 14:12:16 +0200

    fixed integer data types (I was wrong) and fixed nbr_threads function

--- cell_distances.c
+++ cell_distances.c
@@ -30,14 +30,16 @@ int parse_num_threads(int argc, char *argv[]) {
 	} else {
 		printf("Using default number of threads, %d.\n", n_threads);
 	}
+
+	return n_threads;
 }
 
 void determine_file_and_buffer_size(
 				FILE* fp, 
-				int* file_size, 
-				int* buffer_size, 
-				int* nbr_buffer, 
-				int* nbr_lines
+				unsigned long* file_size, 
+				unsigned int* buffer_size, 
+				unsigned int* nbr_buffer, 
+				unsigned int* nbr_lines
 				)
 {
 	// Get file size
@@ -61,26 +63,26 @@ void determine_file_and_buffer_size(
 	*nbr_buffer = *file_size / *buffer_size; 
 	*nbr_lines = *buffer_size / BYTES_PER_LINE;
 
-	printf("File size: %d bytes\n", *file_size);
-	printf("Buffer size: %d bytes\n", *buffer_size);
-	printf("Number of buffers: %d\n", *nbr_buffer);
-	printf("Number of lines per buffer: %d lines\n", *nbr_lines);
+	printf("File size: %lu bytes\n", *file_size);
+	printf("Buffer size: %u bytes\n", *buffer_size);
+	printf("Number of buffers: %u\n", *nbr_buffer);
+	printf("Number of lines per buffer: %u lines\n", *nbr_lines);
 }
 
-void count_distances_within(Coordinate* coords, int nbr_coords, unsigned int* count_distances) {
+void count_distances_within(Coordinate* coords, unsigned int nbr_coords, unsigned long* count_distances) {
 	// #pragma omp parallel for collapse(2) reduction(+:count_distances[:3465])
-	for (long int ix = 0; ix < nbr_coords - 1; ++ix) {
-		for (long int jx = ix + 1; jx < nbr_coords; ++jx) {
+	for (unsigned int ix = 0; ix < nbr_coords - 1; ++ix) {
+		for (unsigned int jx = ix + 1; jx < nbr_coords; ++jx) {
 			short dist = euc_distance(coords[ix], coords[jx]);
 			count_distances[dist] += 1;
 		}
 	}
 }
 
-void count_distances_between(Coordinate* coords1, Coordinate* coords2, int nbr_coords1, int nbr_coords2, unsigned int* count_distances) {
+void count_distances_between(Coordinate* coords1, Coordinate* coords2, unsigned int nbr_coords1, unsigned int nbr_coords2, unsigned long* count_distances) {
 	// #pragma omp parallel for collapse(2) reduction(+:count_distances[:3465])
-	for (long int ix = 0; ix < nbr_coords1; ++ix) {
-		for (long int jx = 0; jx < nbr_coords2; ++jx) {
+	for (unsigned int ix = 0; ix < nbr_coords1; ++ix) {
+		for (unsigned int jx = 0; jx < nbr_coords2; ++jx) {
 			short dist = euc_distance(coords1[ix], coords2[jx]);
 			count_distances[dist] += 1;
 		}
@@ -103,10 +105,11 @@ int main(int argc, char *argv[]){
 	}
 
 	// Get file and buffer size
-	int file_size, buffer_size, nbr_buffer, nbr_lines;
+	unsigned long file_size; // our maximum filesize is (2^32)*24 (UINT_MAX * BYTES_PER_LINE) > UINT_MAX
+	unsigned int buffer_size, nbr_buffer, nbr_lines;
 	determine_file_and_buffer_size(fp, &file_size, &buffer_size, &nbr_buffer, &nbr_lines);
-	int remaining_bytes = file_size % buffer_size;
-	int remaining_lines = remaining_bytes / BYTES_PER_LINE;
+	unsigned int remaining_bytes = file_size % buffer_size;
+	unsigned int remaining_lines = remaining_bytes / BYTES_PER_LINE;
 
 	// Allocate arrays
 	char *buffer = (char*)malloc(buffer_size);
@@ -117,13 +120,14 @@ int main(int argc, char *argv[]){
 		fclose(fp);
 		return 1;
 	}
-	unsigned int count_distances[MAX_DISTANCE+1] = {0}; //Array to store all the possible distances (from 0 to 3464).
+	unsigned long count_distances[MAX_DISTANCE+1] = {0}; //Array to store all the possible distances (from 0 to 3464).
+	// maximum counts is (2^32)*(2^32 - 1)/2 > (UINT_MAX), even if it is very unlikely
 
 	///////////////////////////////////////////
 	// Main computations
 
 	// Move the "fixed" buffer -> If needed could try reversed sweep but complicated
-	for (int i_fixed = 0; i_fixed < nbr_buffer; i_fixed++){
+	for (unsigned int i_fixed = 0; i_fixed < nbr_buffer; i_fixed++){
 		// Read fixed buffer
 		read_coordinates(fp, (i_fixed * buffer_size), coords_fixed, buffer, buffer_size);
 
@@ -132,7 +136,7 @@ int main(int argc, char *argv[]){
 
 		// Scan over the rest of the file
 		// (if i_fixed is the last one this for loop is automatically skipped)
-		for (int i_scan = i_fixed + 1; i_scan < nbr_buffer; i_scan++) {
+		for (unsigned int i_scan = i_fixed + 1; i_scan < nbr_buffer; i_scan++) {
 			read_coordinates(fp, (i_scan * buffer_size), coords_scan, buffer, buffer_size);
 			count_distances_between(coords_fixed, coords_scan, nbr_lines, nbr_lines, count_distances);
 		}
@@ -150,14 +154,14 @@ int main(int argc, char *argv[]){
 	///////////////////////////////////////////
 	
 	// Check if we counted all pairs
-	unsigned int total_count_distances = 0;
-	unsigned int total_lines = file_size/24;
-	for (int ix = 0; ix < MAX_DISTANCE + 1; ix++) {
+	unsigned long total_count_distances = 0;
+	unsigned long total_lines = file_size/24;
+	for (unsigned int ix = 0; ix < (MAX_DISTANCE + 1); ix++) {
 		total_count_distances += count_distances[ix];
-		printf("%05.2f %u\n", ((float)ix)/100, count_distances[ix]);
+		printf("%05.2f %lu\n", ((float)ix)/100, count_distances[ix]);
 	}
-	printf("Counted distances: %u\n", total_count_distances);
-	printf("Total distances: %u\n", (total_lines-1) * (total_lines) / 2); // Sum of n-1 integers
+	printf("Counted distances: %lu\n", total_count_distances);
+	printf("Total distances: %lu\n", (total_lines-1) * (total_lines) / 2); // Sum of n-1 integers
 
 	// Free stuff
 	fclose(fp);
--- constants.h
+++ constants.h
@@ -4,8 +4,8 @@
 #define BYTES_PER_LINE (3 * FLOAT_LENGTH)
 #define COORD_NBR 1
 #define MAX_DISTANCE 3464
-#define MAX_MEM 4500000 //Ideally 4.5Mb, change that when it works.
-
+#define MAX_MEM 4500000 //Ideally 4.5Mb, change that when it works. 
+// MAX_MEM has to be smaller than 4294967296 (UINT_MAX)
 
 #define INPUT_FILE "data/cells_1e5"
 
