2b70ae90cca34be3cb84cb1c6a2a41236bc6c2a0
Nico Guth <nicog@chalmers.se>
2023-10-06 17:06:50 +0200

    hardcoded color strings + fwrite (much faster)

--- A3/write_ppm.h
+++ A3/write_ppm.h
@@ -12,21 +12,31 @@ typedef struct {
 #define CHARS_PER_CHANNEL 4
 #define CHARS_PER_PIXEL (3 * CHARS_PER_CHANNEL)
 
-RGBColor colors[10] = {
-    {0, 0, 0},       // Black (not converged)
-    {255, 0, 0},     // Red
-    {0, 255, 0},     // Green
-    {0, 0, 255},     // Blue
-    {255, 128, 0},   // Orange
-    {128, 0, 128},   // Purple
-    {255, 255, 0},   // Yellow
-    {0, 255, 255},   // Cyan
-    {255, 0, 255},   // Magenta
-    {128, 128, 128}  // Gray
+char color_strings[10][CHARS_PER_PIXEL+1] = {
+    "000 000 000 ",   // Black (not converged)
+    "255 000 000 ",   // Red
+    "000 255 000 ",   // Green
+    "000 000 255 ",   // Blue
+    "255 128 000 ",   // Orange
+    "128 000 128 ",   // Purple
+    "255 255 000 ",   // Yellow
+    "000 255 255 ",   // Cyan
+    "255 000 255 ",   // Magenta
+    "128 128 128 "    // Gray
 };
+
+char greyscale_strings[MAX_COLOR_VALUE+1][CHARS_PER_PIXEL+1];
+
+static inline
+void prepare_greyscale_strings() {
+    for (int i = 0; i < MAX_COLOR_VALUE+1; i++) {
+        sprintf(greyscale_strings[i], "%03d %03d %03d\n", i, i, i);
+    }
+}
 
 static inline
 void write_ppm_header(FILE* file) {
+    prepare_greyscale_strings(); // maybe not the best place to put it
     fprintf(file, "P3\n");
     fprintf(file, "%d %d\n", image_size, image_size);
     fprintf(file, "%d\n", MAX_COLOR_VALUE);
@@ -34,29 +44,20 @@ void write_ppm_header(FILE* file) {
 
 static inline
 void write_attractors_row(FILE *file, short *root_idxs_row) {
-    int str_length = CHARS_PER_PIXEL * image_size;
-    char *str_row = (char*) malloc(sizeof(char) * (str_length + 1)); // +1 for the string terminating character
-
     for (int jx = 0; jx < image_size; jx++) {
-        RGBColor color = colors[root_idxs_row[jx] + 1];
-        sprintf(str_row + jx*CHARS_PER_PIXEL, "%03d %03d %03d\n", color.r, color.g, color.b);
+        char *color_str = color_strings[root_idxs_row[jx] + 1];
+        fwrite(color_str, sizeof(char), CHARS_PER_PIXEL, file);
     }
-
-    fwrite(str_row, sizeof(char), str_length, file);
-    free(str_row);
 }
+
 
 static inline
 void write_convergence_row(FILE *file, short *n_its_row) {
-    int str_length = CHARS_PER_PIXEL * image_size;
-    char *str_row = (char*) malloc(sizeof(char) * (str_length + 1)); // +1 for the string terminating character
-
     for (int jx = 0; jx < image_size; jx++) {
         int greyscale_value = (((double)n_its_row[jx]) / MAX_ITERATIONS) * MAX_COLOR_VALUE;
-        sprintf(str_row + jx*CHARS_PER_PIXEL, "%03d %03d %03d\n", greyscale_value, greyscale_value, greyscale_value);
+        char *greyscale_str = greyscale_strings[greyscale_value];
+        fwrite(greyscale_str, sizeof(char), CHARS_PER_PIXEL, file);
     
     }
-    fwrite(str_row, sizeof(char), str_length, file);
-    free(str_row);
 }
 
